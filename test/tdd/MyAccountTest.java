140a69fa0f26233bb77604c2ec0fcf5dc13d88b8
Samson Aderibigbe <samsontimilehin@gmail.com>
2021-05-28 08:39:50 +0100

    ChapterFive

--- test/tdd/MyAccountTest.java
+++ test/tdd/MyAccountTest.java
@@ -1,10 +1,12 @@
 package tdd;
 
+import chapterFour.Account;
 import chapterFour.Kata;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MyAccountTest {
 
@@ -41,49 +43,50 @@ public class MyAccountTest {
         assertEquals(0, account.getBalance());
     }*/
 
-        @Test
-    void checkThatAccountCanDeposit(){
-            MyAccount account = new MyAccount();
-            account.deposit(2500);
-            assertEquals(2500, account.getBalance());
-        }
+     //   @Test
+   // void checkThatAccountCanDeposit(){
+      //      MyAccount account = new MyAccount();
+       //     account.deposit(2500);
+       //     assertEquals(2500, account.getBalance());
+      //  }
 
-        @Test
-        @DisplayName("Money can be deposited twice test")
-    void accountCanReceiveDoubleDeposit(){
-            MyAccount account = new MyAccount();
-            account.deposit(2500);
-            account.deposit(3500);
-            assertEquals(6000, account.getBalance());
-        }
-        @Test
-    public void accountCannotReceiveNegativeDeposit(){
-            MyAccount account = new MyAccount();
-            account.deposit(-9702973);
-            assertEquals(0, account.getBalance());
-        }
-    @Test
-    public void accountCanBeWithdrawnFromAccountWithValidPIn(){
-        MyAccount account = new MyAccount();
-        account.deposit(7000);
-        account.setPin(1234);
-        account.withdraw(2000, 2212);
-        assertTrue(account.getBalance() == 7000);
+     //   @Test
+     //   @DisplayName("Money can be deposited twice test")
+    //void accountCanReceiveDoubleDeposit(){
+        //    MyAccount account = new MyAccount();
+        //    account.deposit(2500);
+         //   account.deposit(3500);
+        //    assertEquals(6000, account.getBalance());
+       // }
+     //   @Test
+   // public void accountCannotReceiveNegativeDeposit(){
+          //  MyAccount account = new MyAccount();
+          //  account.deposit(-9702973);
+           // assertEquals(0, account.getBalance());
+     //   }
+    //@Test
+    //public void accountCanBeWithdrawnFromAccountWithValidPIn(){
+     //   MyAccount account = new MyAccount();
+     //   account.deposit(7000);
+     //   account.setPin(1234);
+    //    account.withdraw(2000, 2212);
+    //    assertTrue(account.getBalance() == 7000);
 
 
-    }
+   // }
 
-    private void assertTrue(boolean condition) {
-    }
+    //private void assertTrue(boolean condition) {
+   // }
        // @Test
     //void purchaseOneUnitOfDrillerTest(){
           //  Kata testDriller = new Kata();
          //   assertEquals(1500, testDriller.calculatePriceFor(testDriller 1));
       //  }
-    @Test
-    public void userCanNotWithdrawAllBalance(){
-        MyAccount account = new MyAccount();
-            account.setFixBalance(1000);
-            assertEquals(1000, account.getBalance());
-    }
+    //@Test
+  //  public void userCanNotWithdrawAllBalance(){
+    //    MyAccount account = new MyAccount();
+    //        account.setFixBalance(1000);
+    //        assertEquals(1000, account.getBalance());
+   // }
+
 }
